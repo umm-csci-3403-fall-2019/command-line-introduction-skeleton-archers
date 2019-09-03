be22f3cf24934252a591920d7d62429609bc6af5
Jackson Tenor <tenor031@umn.edu>
2019-09-03 11:06:52 -0500

    Add comments to improve code clarity

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,9 +1,13 @@
 tar xf $1 -C $2
+#Store the starting directory 
 ogdir=$(pwd)
 cd $2
+#find all files with delte me
 files=$(grep -Rl 'DELETE ME!')
+#and delete them
 for file in "${files}"
 do
 	rm $file
 done
+#Compress the cleaned folder into the starting directory
 tar -czf "$ogdir/cleaned_$1" $(basename $1 .tgz)
--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,7 +1,7 @@
+#make a directory and extract into it.
 mkdir -p $2
 tar xf NthPrime.tgz -C $2
-
+#Compile the c code
 gcc -o $2/NthPrime/NthPrime $2/NthPrime/main.c $2/NthPrime/nth_prime.c
-
-
+#Run the c code
 $2/NthPrime/NthPrime $1
